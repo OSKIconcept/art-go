f9a97bc3d471551592758892590b24922503b659
oskiconcept <oscarjoseph7991@gmail.com>
2024-07-09 19:43:42 +0100

    font update

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -186,7 +186,7 @@ const CheckoutPage = () => {
 
       <div className="items-center justify-center flex font-clash md:pb-12 pb-6">
         <div className="text-white md:px-8 px-4 md:py-3 py-1.5 bg-black">
-          <h3>Pay $6000</h3>
+          <h3 className="font-clash">Pay $6000</h3>
         </div>
       </div>
     </div>
